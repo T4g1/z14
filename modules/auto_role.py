2cc1c8ba53b315da16d4c44d1e13abe39ef261b7
T4g1 <thomasvangysegem@gmail.com>
2021-01-28 16:18:44 +0100

    [fix] Resilience on self_roles

--- modules/auto_role.py
+++ modules/auto_role.py
@@ -12,6 +12,7 @@ class AutoRole(commands.Cog):
     """
     def __init__(self, bot):
         self.bot = bot
+        self.role = None
 
 
     def test(self):
@@ -19,13 +20,31 @@ class AutoRole(commands.Cog):
 
 
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        """ Gives new member a pre-defined role
+    async def on_ready(self):
+        """
+        Check every users has at least the role to them
         """
         auto_role = os.getenv("AUTO_ROLE", default="Joueur")
 
-        role = discord.utils.get(member.guild.roles, name=auto_role)
-        if role:
-            await member.add_roles(role)
-        else:
-            print("AUTO_ROLE not found! Create a role named {}".format(auto_role))
+        self.role = discord.utils.get(
+            self.bot.get_guild().roles, name=auto_role)
+
+        if not self.role:
+            raise Exception(
+                "AUTO_ROLE not found! Create a role named {}".format(auto_role))
+
+        for member in self.bot.get_guild().members:
+            if len(member.roles) == 1:
+                await self.give_role(member)
+
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        """ Gives new member a pre-defined role
+        """
+        await self.give_role(member)
+
+
+    async def give_role(self, member):
+        print("Giving role {} to {}".format(self.role.name, member.name))
+        await member.add_roles(self.role)
