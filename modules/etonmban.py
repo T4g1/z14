7af1385c1281e616910bdf1c590ae1aee806e365
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2021-01-30 13:52:58 +0100

    [enh] It works

--- modules/etonmban.py
+++ modules/etonmban.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import discord
 
 from discord.ext import commands
@@ -12,36 +13,38 @@ class EtOnMBan(commands.Cog):
         self.bot = bot
         self.ban_url = os.getenv("BAN_URL")
         self.voice_client = None
-        self.sample = discord.FFmpegPCMAudio(os.getenv("BAN_URL"))
 
 
     def test(self):
         assert not os.getenv("BAN_URL") is None, \
             "BAN_URL is not defined"
 
-        assert discord.FFmpegPCMAudio(os.getenv("BAN_URL")), \
-            "Audio sample for etonmban could not be loaded"
-
-
-    async def on_play_end(error):
-        """ Disconnects bot from voice channel
-        """
-        if not self.voice_client:
-            return
-
-        await self.voice_client.disconnect()
+        try:
+            sample = discord.FFmpegPCMAudio(os.getenv("BAN_URL"))
+        except:
+            self.fail("Audio sample for etonmban could not be loaded")
 
 
     @commands.command()
     async def ban(self, ctx):
         """ Plays a nice audio sample in voice chat
         """
-        if not ctx.author.voice:
-            pass #return await ctx.send("You must be in a voice chat to use this command!")
+        if len(self.bot.voice_clients) > 0:
+            return await ctx.send("I'm busy right now, try later...")
 
-        # DEBUG
-        channel = self.bot.get_channel(804323830696640527) #ctx.author.voice.channel
+        if not ctx.author.voice:
+            return await ctx.send("You must be in a voice chat to use this command!")
+
+        channel = ctx.author.voice.channel
         self.voice_client = await channel.connect()
 
-        self.voice_client.play(self.sample, on_play_end)
+        sample = discord.FFmpegPCMAudio(os.getenv("BAN_URL"))
+        self.voice_client.play(sample)
+
+        await ctx.send("Et on m'ban!")
+
+        while self.voice_client.is_playing():
+            await asyncio.sleep(1)
+
+        await self.voice_client.disconnect()
 
