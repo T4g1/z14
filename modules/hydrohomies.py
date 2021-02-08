69713f32399cddf9eeb6f006fa12afc870302bfa
Greenlamp <gabriel.knuts@gmail.com>
2021-02-08 23:28:01 +0100

    get latest img from subreddit /r/HydroHomies

--- modules/hydrohomies.py
+++ modules/hydrohomies.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
 
+import aiohttp
+import discord
 from discord.ext import commands, tasks
 
 
@@ -24,7 +26,7 @@ class HydroHomies(commands.Cog):
             self.fail("HYDRO_TIMER must be an integer")
 
         try:
-            message_id = int(os.getenv("HYDRO_CHANNEL", default="0"))
+            channel_id = int(os.getenv("HYDRO_CHANNEL", default="0"))
             assert self.bot.get_guild().get_channel(channel_id) is None, \
                 "Given HYDRO_CHANNEL does not exists in this guild"
         except Exception as e:
@@ -40,15 +42,36 @@ class HydroHomies(commands.Cog):
         self.hydrohomies. change_interval(seconds=timer)
         self.hydrohomies.start()
 
+    async def get_latest_img_from_api(self):
+        session = aiohttp.ClientSession()
+        q = await session.get("https://www.reddit.com/r/HydroHomies/new/.json?count=5")
+        r = await q.json()
+        await session.close()
+        url_img = None
+        i = 0
+        while url_img is None and i < 5:
+            data = r['data']['children'][i]['data']
+            if 'url_overridden_by_dest' in data:
+                url_img = data['url_overridden_by_dest']
+            else:
+                i += 1
+        return url_img
+
 
     @tasks.loop(seconds=10)
     async def hydrohomies(self):
         async with self.lock:
+            embed = discord.Embed()
+            url_img = await self.get_latest_img_from_api()
+            if not url_img:
+                embed = None
+            else:
+                embed.set_image(url=url_img)
             await self.channel.send("Don't forget to drink water!\n" \
                 "Next reminder: {}".format(
                     self.hydrohomies.next_iteration.strftime(
                         "%d/%m/%Y %H:%M:%S UTC")
-            ))
+            ), embed=embed)
 
 
 
